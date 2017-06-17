f252a750fe5f468f19e896a8f31c65822ff91b85
ddfridley <ddfridley@yahoo.com>
2017-06-16 20:43:57 -0700

    created index.js and ./src

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const {ReactActionStatePath, ReactActionStatePathClient} = require('./react-action-state-path');
+
+module.exports.ReactActionStatePath=ReactActionStatePath;
+module.exports.ReactActionStatePathClient=ReactActionStatePathClient;
+
+export default ReactActionStatePath;
+
+
