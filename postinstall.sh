181abe233a1824aa9829695f41d4d8944a94fe7e
ddfridley <ddfridley@yahoo.com>
2017-07-28 00:27:05 -0700

    building demo directory for jsfiddle

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
-mkdir -p dist/assets
+mkdir -p dist/demo
 babel --out-dir dist src &&
-browserify dist/demo.js -o dist/assets/demo.js && 
-cp src/demo.html dist && 
-cp src/demo.css dist
+browserify dist/demo.js -o dist/demo/demo.js && 
+browserify dist/react-action-state-path.js -o dist/demo/react-action-state-path.js &&
+cp src/demo.html dist/demo && 
+cp src/demo.css dist/demo
 
